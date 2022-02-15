247f38567726257bc70d567e5fd28f9b23248e7f
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2022-02-15 16:36:06 +0530

    Seeder Update
    : Included admin seeder

---
+++ database/seeders/AdminUser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class AdminUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@app.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'card' => Str::random('16'),
+            'cardcvv' => Str::random('4'),
+        ]);
+
+        $role = Role::create(['name' => 'admin']);
+        $user->assignRole('admin');
+        $role = Role::create(['name' => 'customer']);
+
+    }
+}
