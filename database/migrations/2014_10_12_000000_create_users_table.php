65dc765911089ea470e7d41d6e9a136097009678
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2022-02-15 14:53:27 +0530

    Update User Migration
    : Added Column avatar for default.png

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('card', 16)->unique();
             $table->string('cardcvv', 4);
+            $table->string('avatar')->default('default.png');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
