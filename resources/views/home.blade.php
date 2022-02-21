a791c111e6a85507ddc61a532b34bc111513cd0f
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2022-02-21 21:23:20 +0530

    Update Home Blade View

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,23 @@
 Home
 @endsection
 
-@section('content')
+@section('content')@if(session('status'))
+<div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+    <div class="toast" role="status" aria-live="polite" data-bs-autohide="true" data-bs-animation="true" data-bs-delay="10000">
+        <div class="toast-header">
+            <img src="{{ asset('img/logo.ico') }}" class="rounded img-fluid me-2" alt="logo" height="30" width="30">
+            <strong class="me-auto">Cafeteria</strong>
+            <small>{{ date('s') }}&nbsp;secs ago</small>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body text-success">
+            <i class="bi bi-check2-circle text-success"></i>&nbsp;&nbsp;{{ session('status') }}
+        </div>
+    </div>
+</div>
+@endif
 <div class="container">
-    <h1 class="display-6 fw-bold"><span class="d-inline-block me-3">👋</span>Hi, {{Auth::user()->name}}!</h1>
+    <h1 class="display-6 fs-1 fw-bold mb-3"><span class="d-inline-block me-3">👋</span>Hi, {{Auth::user()->name}}!</h1>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
