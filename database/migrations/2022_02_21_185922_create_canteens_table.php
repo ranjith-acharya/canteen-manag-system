27905181dca7505bf845db9f6e9be239444780b7
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2022-02-21 21:21:26 +0530

    Canteen
    : Controller, Model, Migration
    : Admin can create Canteen

---
+++ database/migrations/2022_02_21_185922_create_canteens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCanteensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('canteens', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('status', ['active', 'in active'])->default('in active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('canteens');
+    }
+}
