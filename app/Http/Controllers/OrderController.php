f3948a92ffbc2affe7bf1354fe66dbf33e3db5b3
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2022-02-27 22:39:04 +0530

    Update Order Controller
    : Notify the Admin when new Order is Placed
    : Notify the Customer when changes has been made for the particular Order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\User;
+use App\Notifications\OrderPlaced;
+use App\Notifications\Status;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
 
 class OrderController extends Controller
@@ -45,6 +49,21 @@ class OrderController extends Controller
         $fields['customer_id'] = Auth::id();
         $fields['reference'] = Str::random('20');
         $fields['total'] = $request->count * $request->price;
+        $fields['customer_status'] = 'ordered';
+        $fields['status']= 'received';
+
+        $admins = User::whereHas(
+            'roles', function($q){
+                $q->where('name', 'admin');
+            }
+        )->pluck('id');
+        // return $admins;
+        foreach($admins as $admin){
+            User::findOrFail($admin)->notify(new OrderPlaced($request->name, Auth::user()->name, $fields['reference']));
+            Session::flash('status', 'New Order!');
+            // return Auth::user()->name;
+        }
+        
         // return $fields;
         Order::create($fields);
         return redirect()->route('canteen.show', $request->canteen_id)->with('status', 'Order Placed!');
@@ -58,7 +77,8 @@ class OrderController extends Controller
      */
     public function show($id)
     {
-        //
+        $order = Order::findOrFail($id);
+        return view('order', compact('order'));
     }
 
     /**
@@ -93,5 +113,38 @@ class OrderController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function setStatus(Request $request){
+        // return $request;
+        $this->validate($request, [
+            'status' => 'required',
+        ]);
+        $order = Order::findOrFail($request->order_id);
+        $fields = $request->all();
+        if($fields['status'] == 'received'){
+            $fields['customer_status'] = 'ordered';
+            $order->user->notify(new Status($fields['customer_status'], route('order.show', $request->order_id)));
+        }
+        if($fields['status'] == 'in-progress'){
+            $fields['customer_status'] = 'in-progress';
+            $order->user->notify(new Status($fields['customer_status'], route('order.show', $request->order_id)));
+        }
+        if($fields['status'] == 'on-the-way'){
+            $fields['customer_status'] = 'on-the-way';
+            $order->user->notify(new Status($fields['customer_status'], route('order.show', $request->order_id)));
+        }
+        if($fields['status'] == 'delivered'){
+            $fields['customer_status'] = 'delivered';
+            $order->user->notify(new Status($fields['customer_status'], route('order.show', $request->order_id)));
+        }
+        if($fields['status'] == 'cancelled'){
+            $fields['customer_status'] = 'cancelled';
+            $order->user->notify(new Status($fields['customer_status'], route('order.show', $request->order_id)));
+        }
+        // return $fields;
+        // return $order;
+        $order->update($fields);
+        return redirect()->route('admin.order.show', $request->order_id)->with('status', 'Status Changed!');
     }
 }
