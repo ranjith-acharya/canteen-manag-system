5d83639d9f4ed18cace3ccf68be8434ebe9298a1
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2022-02-27 22:44:14 +0530

    Update Application Routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\CanteenController;
 use App\Http\Controllers\Admin\FoodItemController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ProfileController;
@@ -26,7 +27,8 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('profile', ProfileController::class);
 Route::resource('/canteen', App\Http\Controllers\CanteenController::class);
-Route::resource('/order', App\Http\Controllers\OrderController::class);
+Route::resource('/order', OrderController::class);
+Route::get('/markRead', [App\Http\Controllers\Admin\NotificationController::class, 'markRead'])->name('markRead');
 
 Route::group(['middleware' => ['role:admin']], function () {
     Route::prefix('admin')->group(function () {
@@ -34,11 +36,16 @@ Route::group(['middleware' => ['role:admin']], function () {
             Route::get('/home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('home');
             Route::resource('profile', ProfileController::class);
 
+            Route::resource('order', OrderController::class);
+            Route::post('order/status', [App\Http\Controllers\OrderController::class, 'setStatus'])->name('set.order.status');
+
             Route::resource('canteen', CanteenController::class);
             Route::post('canteen/status', [App\Http\Controllers\Admin\CanteenController::class, 'setStatus'])->name('set.status');
             Route::resource('food', FoodItemController::class);
             Route::get('/customer/{customer}', [App\Http\Controllers\Admin\CustomerController::class, 'show'])->name('customer.show');
             Route::post('/customer/store', [\App\Http\Controllers\Admin\CustomerController::class, 'store'])->name('customer.store');
+
+            Route::get('/markRead', [App\Http\Controllers\Admin\NotificationController::class, 'markRead'])->name('markRead');
         });
     });
 });
