42199d7d33e3a24c73f753cac72355c61cbd8f9c
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2022-02-27 16:48:27 +0530

    Order Migration Table

---
+++ database/migrations/2022_02_27_133954_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedFloat('price');
+            $table->enum('type', ['veg', 'non-veg']);
+            $table->unsignedDecimal('count');
+            $table->unsignedDecimal('total');
+            $table->string('reference');
+            
+            $table->foreignId('customer_id');
+            $table->foreign('customer_id')->references('id')->on('users');
+            $table->foreignId('canteen_id');
+            $table->foreign('canteen_id')->references('id')->on('canteens');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
