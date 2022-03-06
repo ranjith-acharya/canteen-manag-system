48a7b3d4d6a0db04e42c9fb39a548fa052215bfe
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2022-03-06 14:16:22 +0530

    Created Offer Migration

---
+++ database/migrations/2022_03_06_121808_create_offers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedDecimal('percentage');
+            $table->string('code')->unique();
+            $table->string('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
