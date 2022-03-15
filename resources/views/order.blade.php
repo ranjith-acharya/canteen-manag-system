bd62146b41a72b594f7ff4f8b076222952a50a4a
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2022-03-15 14:15:55 +0530

    Customer Order Layout Change

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -5,21 +5,6 @@ Order Details
 @endsection
 
 @section('content')
-@if(session('status'))
-<div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-    <div class="toast" role="status" aria-live="polite" data-bs-autohide="true" data-bs-animation="true" data-bs-delay="10000">
-        <div class="toast-header">
-            <img src="{{ asset('img/logo.ico') }}" class="rounded img-fluid me-2" alt="logo" height="30" width="30">
-            <strong class="me-auto">Cafeteria</strong>
-            <small>{{ date('s') }}&nbsp;secs ago</small>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body text-success">
-            <i class="bi bi-check2-circle text-success"></i>&nbsp;&nbsp;{{ session('status') }}
-        </div>
-    </div>
-</div>
-@endif
 <div class="container">
     <h4 class="display-6 fs-1 mb-2">Order Details 🍴
         @if(Auth::user()->role === 'customer')
